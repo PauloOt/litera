bf4a73a9cc093a08f9a32f087954aeb1ef7d1980
Paulo Otavio Santos de Jesus <paulootavios123@gmail.com>
2026-04-06 10:40:04 -0300

    feat: configuração inicial do projeto Spring Boot

---
+++ litera-api/src/main/java/com/litera/LiteraApiApplication.java
@@ -0,0 +1,13 @@
+package com.litera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraApiApplication.class, args);
+	}
+
+}
---
+++ litera-api/src/test/java/com/litera/LiteraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
